b302beb6a8aa68312b7c1d94710553b4c92488c2
Felipe Batista <felipebatista47@hotmail.com>
2024-02-07 14:25:49 -0300

    intro Details styling

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,14 @@
 import Intro from '@/components/home/intro/Intro';
+import IntroDetails from '@/components/home/introDetails/IntroDetails';
 
 /* eslint-disable tailwindcss/no-custom-classname */
 export default function Home() {
   return (
-    <section className="mt-6 flex w-full items-center justify-center">
+    <section className="mt-6 flex w-full flex-col items-center justify-center">
       <div className=" flex w-full justify-center bg-dark-100">
         <Intro />
       </div>
+      <IntroDetails />
     </section>
   );
 }
--- components/home/intro/Intro.tsx
+++ components/home/intro/Intro.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Intro = () => {
   return (
-    <section className="flex max-w-[1008px] flex-col items-center justify-center gap-6 px-10 py-24 text-dark-400 tablet:py-40 medium:px-0">
+    <section className="flex max-w-[1008px] flex-col items-center justify-center gap-6 px-10 py-20 text-dark-400 tablet:py-40 medium:px-0">
       <h1 className="font-mukta text-3xl font-semibold  tablet:text-5xl ">
         Helcy Moreira Martins Aguiar
       </h1>
